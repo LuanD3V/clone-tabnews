16134de03166b95ac7134706f04d472fcef4246e
LuanD3V <luan.h.felipe@icloud.com>
2023-10-12 11:15:19 -0300

    feat: add `/api/v1/status` endpoint

---
+++ tests/integrations/api/v1/status/get.test.js
@@ -0,0 +1,4 @@
+test("Get to /api/v1/status should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
+});
---
+++ tests/unit/home.test.js
@@ -0,0 +1,3 @@
+test("Teste de exemplo", () => {
+  expect(1).toBe(1);
+});
