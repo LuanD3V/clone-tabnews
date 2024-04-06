e40bf9b5fd3deb4e4158fa7932d6419f87f3f4ea
LuanD3V <luan.h.felipe@icloud.com>
2024-04-06 18:04:42 -0300

    feat: Adicionando info sobre o banco de dados na resposta da API

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,30 @@
-function status(request, response) {
+import database from "infra/database";
+
+async function status(request, response) {
   const updateAt = new Date().toISOString();
-  response.status(200).json({ status: "OK", update_at: updateAt });
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const databaseName = process.env.POSTGRES_DB;
+  const usedConnectionsResult = await database.query({
+    text: `SELECT COUNT(*)::int AS count FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+  const versionDatabaseResult = await database.query("SHOW server_version;");
+
+  const maxConnections = parseInt(maxConnectionsResult.rows[0].max_connections);
+  const usedConnections = usedConnectionsResult.rows[0].count;
+  const versionDatabase = versionDatabaseResult.rows[0].server_version;
+
+  response.status(200).json({
+    status: "OK",
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        max_connections: maxConnections,
+        used_connections: usedConnections,
+        version: versionDatabase,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -9,3 +9,21 @@ test("Get to /api/v1/status should return update date", async () => {
   const parseUpdateAt = new Date(json.update_at).toISOString();
   expect(json.update_at).toEqual(parseUpdateAt);
 });
+
+test("Get to /api/v1/status should return max connections", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const json = await response.json();
+  expect(json.dependencies.database.max_connections).toEqual(100);
+});
+
+test("Get to /api/v1/status should return used connections", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const json = await response.json();
+  expect(json.dependencies.database.used_connections).toEqual(1);
+});
+
+test("Get to /api/v1/status should return database version", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const json = await response.json();
+  expect(json.dependencies.database.version).toEqual("16.0");
+});
