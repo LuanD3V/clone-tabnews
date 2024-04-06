f9567992344b5e556aef1661f21e9f4e935ad7e8
LuanD3V <luan.h.felipe@icloud.com>
2024-04-06 18:03:01 -0300

    refact: Refatorando a query do banco, adicionando try/catch no `database.js`

--- infra/database.js
+++ infra/database.js
@@ -8,10 +8,16 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    return console.error("Error running query", error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
